4e4b93cacdfc502d171b007770a8e95ba791673d
Srivats Venkataraman <Srivats.venkataraman@gmail.com>
2022-03-11 12:42:49 -0800

    Updated template

--- app/src/main/java/com/example/portfolio/MainActivity.java
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
