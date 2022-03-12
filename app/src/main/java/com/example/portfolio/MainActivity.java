d7a4d2926db037d28e1493a736a5d4aed89c6c94
Srivats Venkataraman <Srivats.venkataraman@gmail.com>
2022-03-11 17:51:35 -0800

    Updated source code

--- app/src/main/java/com/example/portfolio/MainActivity.java
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -9,24 +9,16 @@ import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
     private MaterialCardView instaCard;
-    private MaterialCardView linkedinCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instaCard = findViewById(R.id.instaCard);
-        linkedinCard = findViewById(R.id.linkedinCard);
 
         instaCard.setOnClickListener(view -> {
             Intent i = new Intent(getApplicationContext(), Profile.class);
             i.putExtra("url", "https://www.instagram.com/srivats22/");
-            startActivity(i);
-        });
-
-        linkedinCard.setOnClickListener(view -> {
-            Intent i = new Intent(getApplicationContext(), Profile.class);
-            i.putExtra("url", "http://linkedin.com/in/srivatsvenk");
             startActivity(i);
         });
     }
